e7150d1ed923478149072ed34923a06748198430
zleijun <389843954@qq.com>
2018-08-05 18:50:41 +0800

    添加微信第三方验证

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ class User extends Model
     use SoftDeletes;
 	//此为填充,表示哪些数据可添加到数据可
 	protected $fillable = [
-		'name','email','password','phone','remember_token','avatar','introduction','notification_count','last_actived_at','deleted_at'
+		'name','email','password','phone','remember_token','avatar','introduction','notification_count','last_actived_at','deleted_at','weixin_openid','weixin_unionid'
 	];
 	//时间戳化整型
 	// protected $dateFormat = 'U';
